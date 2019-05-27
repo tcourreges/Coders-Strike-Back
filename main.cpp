fa55bf49874659c8c71668cc9fee4cc7a367b8dd
tcourreges <tanguy.courreges@gmail.com>
2019-05-27 13:58:44 +0200

    Default code

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * This code automatically collects game data in an infinite loop.
+ * It uses the standard input to place data into the game variables such as x and y.
+ * YOU DO NOT NEED TO MODIFY THE INITIALIZATION OF THE GAME VARIABLES.
+ **/
+
+int main()
+{
+
+    // game loop
+    while (1) {
+        int x; // x position of your pod
+        int y; // y position of your pod
+        int nextCheckpointX; // x position of the next check point
+        int nextCheckpointY; // y position of the next check point
+        cin >> x >> y >> nextCheckpointX >> nextCheckpointY; cin.ignore();
+
+        // Write an action using cout. DON'T FORGET THE "<< endl"
+        // To debug: cerr << "Debug messages..." << endl;
+
+
+        // Edit this line to output the target position
+        // and thrust (0 <= thrust <= 100)
+        // i.e.: "x y thrust"
+        cout << nextCheckpointY << " " << nextCheckpointY << " 50" << endl;
+
+
+    }
+}
