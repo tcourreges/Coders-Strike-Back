ad03805a15fc448b1828eb760458686f46779ca9
tcourreges <tanguy.courreges@gmail.com>
2019-05-27 14:06:49 +0200

    Wood 2: Default code

--- main.cpp
+++ main.cpp
@@ -6,33 +6,33 @@
 using namespace std;
 
 /**
- * This code automatically collects game data in an infinite loop.
- * It uses the standard input to place data into the game variables such as x and y.
- * YOU DO NOT NEED TO MODIFY THE INITIALIZATION OF THE GAME VARIABLES.
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
  **/
-
-constexpr int maxThrust = 100;
 
 int main()
 {
 
     // game loop
     while (1) {
-        int x; // x position of your pod
-        int y; // y position of your pod
+        int x;
+        int y;
         int nextCheckpointX; // x position of the next check point
         int nextCheckpointY; // y position of the next check point
-        cin >> x >> y >> nextCheckpointX >> nextCheckpointY; cin.ignore();
+        int nextCheckpointDist; // distance to the next checkpoint
+        int nextCheckpointAngle; // angle between your pod orientation and the direction of the next checkpoint
+        cin >> x >> y >> nextCheckpointX >> nextCheckpointY >> nextCheckpointDist >> nextCheckpointAngle; cin.ignore();
+        int opponentX;
+        int opponentY;
+        cin >> opponentX >> opponentY; cin.ignore();
 
         // Write an action using cout. DON'T FORGET THE "<< endl"
         // To debug: cerr << "Debug messages..." << endl;
 
 
-        // Edit this line to output the target position
-        // and thrust (0 <= thrust <= 100)
+        // You have to output the target position
+        // followed by the power (0 <= thrust <= 100)
         // i.e.: "x y thrust"
-        cout << nextCheckpointX << " " << nextCheckpointY << " " << maxThrust << endl;
-
-
+        cout << nextCheckpointX << " " << nextCheckpointY << " 80" << endl;
     }
 }
